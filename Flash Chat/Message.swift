cc9f57bb7734faeefb5146d2cea5d191b6dca87a
Johan <jlindstrom@lifetimefitness.com>
2017-12-30 20:28:33 -0700

    App Works

---
+++ Flash Chat/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  Flash Chat
+//
+//  This is the model class that represents the blueprint for a message
+
+class Message {
+    
+    //TODO: Messages need a messageBody and a sender variable
+    //Properties below are always set as VAR variables
+    
+    var sender: String = ""
+    var messageBody: String = ""
+    
+}
